bf58e45000e89eaba2ee093869e1003594046dbe
AarulKoul <aarulkoul03@gmail.com>
2023-02-21 18:27:44 +0530

    copy button

--- lib/text.dart
+++ lib/text.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'screen2.dart';
+import 'dart:ui';
+
 
 class text extends StatefulWidget {
   const text({Key? key}) : super(key: key);
@@ -43,12 +46,22 @@ class _textState extends State<text> {
             width: double.infinity,
             height: double.infinity,
             color: const Color(0xFFFFE7ED),
-            padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0, bottom: 15.0),
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0, bottom: 15.0),
             child: Expanded(
               child: SingleChildScrollView(
                 child: scannedtext != "" ? Text(scannedtext, style: TextStyle(color: Colors.black, fontSize: 20.0),):Text("Upload image with text first.", style: TextStyle(color: Colors.grey, fontSize: 20.0),),
               ),
             ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Clipboard.setData(ClipboardData(text: scannedtext));
+            },
+            child:
+              Icon(Icons.copy,
+              color: Colors.red,
+              ),
+            backgroundColor: Color(0xFFFFE7ED),
           ),
         )
     );
